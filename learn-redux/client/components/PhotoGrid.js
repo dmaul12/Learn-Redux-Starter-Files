75fc8777a9c1a529804a1ce8ad7cde02768333b9
David Maul <dmaul12@gmail.com>
2016-09-12 15:10:15 -0400

    added comments

--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -6,7 +6,7 @@ const PhotoGrid = React.createClass({
     return (
 
      <div className="photo-grid">
-      {this.props.posts.map((post, i) => <Photo/>)}
+      {this.props.posts.map((post, i) => <Photo{...this.props} key ={i} i={i} post={post}/>)}
      </div>
 
       )
